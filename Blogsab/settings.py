9fddd2572116105fa6fc9b0ab39c4d378368fb1a
Hassan Fiaz <68145207+silverab@users.noreply.github.com>
2020-08-22 07:22:07 +0500

    debug = False

--- Blogsab/settings.py
+++ Blogsab/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q1t*n*vin!*-g!plo)4ge29mv9djzziorvwkhn0^u%g@)9%qiq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
