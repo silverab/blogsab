ed35e8ea0876b274c40b45f84217fe140738aeb5
Hassan Fiaz <68145207+silverab@users.noreply.github.com>
2020-08-22 16:43:54 +0500

    Update views.py

--- sports/views.py
+++ sports/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from datetime import datetime, timedelta
-from sports.models import LPost, RPost, CPost, MiniCPost, AsiaPost
+from sports.models import LPost, RPost, CPost, MiniCPost, AsiaPost, AfricaPost, EuropePost, NorthAPost
 
 def home(request):
+	northAmerica = NorthAPost.objects.all().order_by('-id')
+	europe = EuropePost.objects.all().order_by('-id')
+	africa = AfricaPost.objects.all().order_by('-id')
 	asia = AsiaPost.objects.all().order_by('-id')[:3]
 	rightpost = RPost.objects.all().order_by('-id')[:1]
 	double_rightpost = RPost.objects.all().order_by('-id')[1:3]
@@ -12,7 +15,20 @@ def home(request):
 	center_doublepost = CPost.objects.all()[1:3]
 	minipost = MiniCPost.objects.all()
 	template = 'sports/index.html'
-	context = {'leftposts': left_singlepost, 'left_doublesports': left_doublepost, 'rightposts': rightpost, 'cposts': center_singlepost, 'dobuleposts': center_doublepost, 'miniposts': minipost, 'double_rightposts': double_rightpost, 'asia': asia }
+	# {'leftposts': left_singlepost, 'left_doublesports': left_doublepost, 'rightposts': rightpost, 'cposts': center_singlepost, 'dobuleposts': center_doublepost, 'miniposts': minipost, 'double_rightposts': double_rightpost, 'asia': asia }
+	context = {
+		'northAmerica': northAmerica,
+		'europe': europe,
+		'africa': africa,
+		'dobuleposts': center_doublepost,
+		'miniposts': minipost,
+		'double_rightposts': double_rightpost,
+		'cposts': center_singlepost,
+		'rightposts': rightpost,
+		'left_doublesports': left_doublepost,
+		'leftposts': left_singlepost,
+		'asia': asia
+	}
 	response =  render(request, template, context)
 	response.set_signed_cookie('name', 'User', salt='ab' , expires=datetime.utcnow()+timedelta(days=2))
 	return response
